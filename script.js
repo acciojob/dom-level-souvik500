66514639a86ba40adab3b80fc4f0991897bcc18f
souvik500 <120314952+souvik500@users.noreply.github.com>
2023-06-11 22:05:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+
+// let ul = document.getElementsByTagName("ul")[0];
+
+// for(let i=0;i<ul.children.length;i++){
+// 	if(ul.children[i].id === "level"){
+// 		// let index = i+1;
+// 		alert("The level of the element is: "+i);
+// 	}
+
+document.addEventListener("DOMContentLoaded", function() {
+  let element = document.getElementById("level");
+  let level = calculateDOMLevel(element);
+  alert("The level of the element is: " + level);
+});
+
+function calculateDOMLevel(element) {
+  let level = 1;
+  while (element.parentElement) {
+    element = element.parentElement;
+    level++;
+  }
+  return level;
+}
